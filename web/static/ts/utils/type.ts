d69e8dbc19f81e77384685614ef3b81e1118db24
Mark <1515135+MarkKoz@users.noreply.github.com>
2022-05-24 18:33:45 -0700

    Fix linting deps

--- web/static/ts/utils/type.ts
+++ web/static/ts/utils/type.ts
@@ -1,9 +1,8 @@
 export type GetKeys<T> = {[P in keyof T]: P}[keyof T];
 
-export type MandateProps<T extends {}, U> = Omit<T, GetKeys<U>> &
-    {
-        [Property in keyof U]-?: U[Property];
-    };
+export type MandateProps<T extends {}, U> = Omit<T, GetKeys<U>> & {
+    [Property in keyof U]-?: U[Property];
+};
 
 export type NonUndefinedProps<T> = {
     [Property in keyof T]: T[Property] extends undefined ? never : Property;
